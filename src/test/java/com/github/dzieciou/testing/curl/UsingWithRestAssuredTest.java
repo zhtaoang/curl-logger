7b7ec24de580f1267bfa8c9b39642920b444dc61
mgawinec <mgawinec@akamai.com>
2016-03-11 18:12:52 +0100

    Refactoring.

--- src/test/java/com/github/dzieciou/testing/curl/UsingWithRestAssuredTest.java
+++ src/test/java/com/github/dzieciou/testing/curl/UsingWithRestAssuredTest.java
@@ -7,8 +7,6 @@ import org.apache.http.impl.client.AbstractHttpClient;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.testng.annotations.Test;
 
-import java.io.IOException;
-
 import static com.jayway.restassured.RestAssured.config;
 import static com.jayway.restassured.RestAssured.given;
 import static com.jayway.restassured.config.HttpClientConfig.httpClientConfig;
